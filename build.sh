213d0043e6a889f8e6e0339d7ae6b5e17a32d35b
navin136 <nkwhitehat@gmail.com>
2022-10-11 07:39:39 +0530

    Fix

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ file() {
 	-F "parse_mode=html" \
 	-F caption="$2 | <b>MD5 Checksum : </b><code>$MD5</code>"
 }
-docker login -u navin136 -p $PASSWD || { echo "Docker Login failed" && msg <b>Docker Login Failed<b/> && exit 1; }
-docker build . -t navin136/builder || { echo "Docker Build failed" && msg <b>Docker Build Failed<b/> && exit 1; }
-docker push -t navin136/builder || { echo "Docker Push failed" && msg <b>Docker Push Failed<b/> && exit 1; }
-msg "<b>Docker Built and Uploaded successfully..</b>%0A<b>LInk: </b><a href="https://hub.docker.com/repository/docker/navin136/builder">Click Here</a>"
+docker login -u navin136 -p "$PASSWORD"
+sudo docker build . -t navin136/builder || { echo "Docker Build failed" && msg "<b>Docker Build Failed</b>" && exit 1; }
+docker push -t navin136/builder || { echo "Docker Push failed" && "msg <b>Docker Push Failed</b>" && exit 1; }
+msg "<b>Docker Built and Uploaded successfully..</b>%0A<b>Link: </b><a href="https://hub.docker.com/repository/docker/navin136/builder">Click Here</a>"
