c5e1ff6ef2bd7d168eb5c81fea6913d396b04513
navin136 <nkwhitehat@gmail.com>
2022-10-11 07:32:33 +0530

    Automate building

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+msg() {
+	curl -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id="$CHAT_ID" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+}
+file() {
+	MD5=$(md5sum "$1" | cut -d' ' -f1)
+	curl --progress-bar -F document=@"$1" "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
+	-F chat_id="$CHAT_ID"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$2 | <b>MD5 Checksum : </b><code>$MD5</code>"
+}
+docker login -u navin136 -p $PASSWD || { echo "Docker Login failed" && msg <b>Docker Login Failed<b/> && exit 1; }
+docker build . -t navin136/builder || { echo "Docker Build failed" && msg <b>Docker Build Failed<b/> && exit 1; }
+docker push -t navin136/builder || { echo "Docker Push failed" && msg <b>Docker Push Failed<b/> && exit 1; }
+msg "<b>Docker Built and Uploaded successfully..</b>%0A<b>LInk: </b><a href="https://hub.docker.com/repository/docker/navin136/builder">Click Here</a>"
